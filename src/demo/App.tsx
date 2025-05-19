ce069018ba48299bba82ffc048eee3316064e2f9
Guillermo Pages <mail@guillermo.at>
2025-05-19 14:15:33 +0200

    refactor: indent

--- src/demo/App.tsx
+++ src/demo/App.tsx
@@ -4,7 +4,8 @@ import './App.css';
 import { DaisyPlayerWithRouter } from '../lib';
 
 function App() {
-  const dirUrl = import.meta.env.VITE_BASE_URL + '/';
+  const streamPath = "/unzipped/BBR_13812A100/5b6fc4a84eaac8041668e49658c981a5b4ee820afabc5e612bbf9fea541523bd/1747738070";
+  const dirUrl = `${import.meta.env.VITE_BASE_URL}${streamPath}`;
   const bookmarkRouteParamName = 'bookmark';
   const appUrl = import.meta.env.VITE_APP_URL;
 
